cb864ba9eeb79a52b9ab346403bd5f78aff59b53
Timowest12 <timowester@gmail.com>
2022-01-26 19:55:01 +0100

    rubcop fixes

--- decode.rb
+++ decode.rb
@@ -1,47 +1,47 @@
 def decodeChar(str)
-    @morseChar = {
-        '.-' => 'A',
-        '-...' => 'B',
-        '-.-.' => 'C',
-        '-..' => 'D',
-        '.' => 'E',
-        '..-.' => 'F',
-        '--.' => 'G',
-        '....' => 'H',
-        '..' => 'I',
-        '.---' => 'J',
-        '-.-' => 'K',
-        '.-..' => 'L',
-        '--' => 'M',
-        '-.' => 'N',
-        '---' => 'O',
-        '.--.' => 'P',
-        '--.-' => 'Q',
-        '.-.' => 'R',
-        '...' => 'S',
-        '-' => 'T',
-        '..-' => 'U',
-        '...-' => 'V',
-        '.--' => 'W',
-        '-..-' => 'X',
-        '-.--' => 'Y',
-        '--..' => 'Z',
-    }
-    @morseChar[str]
+  @morse_Char = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  @morse_Char[str]
 end
 
 def decodeWord(str)
-    morse = str.split
-    word = morse.map { |c| decodeChar(c) }
-    word.join
+  morse = str.split
+  word = morse.map { |c| decodeChar(c) }
+  word.join
 end
 
 def decodeSentence(str)
-    morse = str.split('  ')
-    sentence = morse.map { |w| decodeWord(w) }
-    sentence.join(' ')
+  morse = str.split('  ')
+  sentence = morse.map { |w| decodeWord(w) }
+  sentence.join(' ')
 end
-decodeWord(".-") 
-decodeWord("-- -.--")
-decodeSentence("-- -.--   -. .- -- .")
-decodeSentence(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
+decodeWord('.-')
+decodeWord('-- -.--')
+decodeSentence('-- -.--   -. .- -- .')
+decodeSentence('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...')
